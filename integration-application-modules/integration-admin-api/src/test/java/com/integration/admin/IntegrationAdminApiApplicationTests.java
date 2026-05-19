29470418eca9a6be2f848ea24e0a19bd04a61351
tigerchn <lxm.js.chn@gmail.com>
2026-05-19 14:55:43 +0800

    优化代码

--- integration-application-modules/integration-admin-api/src/test/java/com/integration/admin/IntegrationAdminApiApplicationTests.java
+++ integration-application-modules/integration-admin-api/src/test/java/com/integration/admin/IntegrationAdminApiApplicationTests.java
@@ -1,5 +1,8 @@
 package com.integration.admin;
 
+import com.integration.admin.web.auth.AdminAuthController;
+import com.integration.common.encrypt.assistant.PayloadAssistant;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -8,7 +11,13 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class IntegrationAdminApiApplicationTests {
 
+    @Resource
+    private PayloadAssistant payloadAssistant;
+
     @Test
     void contextLoads() {
+        AdminAuthController.LoginRequest request = new AdminAuthController.LoginRequest("admin", "admin123");
+        String encrypt = payloadAssistant.encrypt(request);
+        System.out.println("encrypt = " + encrypt);
     }
 }
